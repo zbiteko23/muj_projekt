ee2e9cdf311e4bdd68dd93a8a9f5e57a5e985658
Jakub Šenkýř <senkyr@spstrutnov.cz>
2024-01-04 10:42:40 +0100

    Uživatelský pohyb prvků obrazu

--- src/main.py
+++ src/main.py
@@ -7,11 +7,15 @@ rozliseni_okna = (800, 600)
 
 okno = pygame.display.set_mode(rozliseni_okna)
 
-pozice_x = 300
-pozice_y = 200
+pozice_micku_x = 300
+pozice_micku_y = 200
 velikost_micku = 50
-rychlost_micku_x = 1
-rychlost_micku_y = 1
+rychlost_micku_x = 0.5
+rychlost_micku_y = 0.5
+
+pozice_hrace_x = 200
+pozice_hrace_y = 100
+rychlost_hrace = 0.2
 
 while True:
     for udalost in pygame.event.get():
@@ -19,26 +23,31 @@ while True:
             pygame.quit()
             sys.exit()
     
-    pozice_x += rychlost_micku_x
-    pozice_y += rychlost_micku_y
+    stisknute_klavesy = pygame.key.get_pressed()
     
-    if pozice_x < 0:
-        pozice_x = 0
+    if stisknute_klavesy[pygame.K_RIGHT]:
+        pozice_hrace_x += rychlost_hrace
+    
+    pozice_micku_x += rychlost_micku_x
+    pozice_micku_y += rychlost_micku_y
+    
+    if pozice_micku_x < 0:
+        pozice_micku_x = 0
         rychlost_micku_x *= -1
-    if pozice_y < 0:
-        pozice_y = 0
+    if pozice_micku_y < 0:
+        pozice_micku_y = 0
         rychlost_micku_y *= -1
     
-    if pozice_x > rozliseni_okna[0] - velikost_micku:
-        pozice_x = rozliseni_okna[0] - velikost_micku
+    if pozice_micku_x > rozliseni_okna[0] - velikost_micku:
+        pozice_micku_x = rozliseni_okna[0] - velikost_micku
         rychlost_micku_x *= -1
-    if pozice_y > rozliseni_okna[1] - velikost_micku:
-        pozice_y = rozliseni_okna[1] - velikost_micku
+    if pozice_micku_y > rozliseni_okna[1] - velikost_micku:
+        pozice_micku_y = rozliseni_okna[1] - velikost_micku
         rychlost_micku_y *= -1
     
     okno.fill((255, 255, 255))
     
-    pygame.draw.rect(okno, (0, 0, 0), (200, 100, 50, 50))
-    pygame.draw.ellipse(okno, (0, 0, 255), (pozice_x, pozice_y, velikost_micku, velikost_micku))
+    pygame.draw.rect(okno, (0, 0, 0), (pozice_hrace_x, pozice_hrace_y, 50, 50))
+    pygame.draw.ellipse(okno, (0, 0, 255), (pozice_micku_x, pozice_micku_y, velikost_micku, velikost_micku))
     
     pygame.display.update()
