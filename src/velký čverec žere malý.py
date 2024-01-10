4485a2565a8a99b497d54b669b98571f765682e8
zbiteko23 <zbiteko23@spstrutnov.cz>
2024-01-10 18:40:27 +0100

    přemyšlim jak udělat random pozici

--- src/velký čverec žere malý.py
+++ src/velký čverec žere malý.py
@@ -3,17 +3,26 @@ import sys
 import pygame
 pygame.init()
 
+import random
+
 #velikost okna
 rozliseni_okna = (800, 800)
 
 okno = pygame.display.set_mode(rozliseni_okna)
 
 #pozice ctverce
-pozice_ctverce_x = 200
+pozice_ctverce_x = 500
 pozice_ctverce_y = 100
 
 #ryclost ctverce
 rychlost_ctverce = 0.2
+
+#nahodna pozice ctevercku
+def random_pozice():
+    x = random.randint(0, -20)
+    y = random.randint(0, -20)
+    return x, y
+
 
 #ukonceni hry
 while True:
@@ -36,6 +45,9 @@ while True:
         
     if stisknute_klavesy[pygame.K_DOWN]:
         pozice_ctverce_y += rychlost_ctverce
+            
+#vytvoreni malych cteverku
+    pygame.draw.rect(okno, (x, y, 15, 15))
         
     
     
@@ -44,5 +56,8 @@ while True:
     
 #vytvoreni ctverce
     pygame.draw.rect(okno, (0, 0, 0), (pozice_ctverce_x, pozice_ctverce_y, 20, 20))
+    
+#vytvoreni malych ctvercu
+    
     
     pygame.display.update()
