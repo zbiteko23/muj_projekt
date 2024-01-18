a3a807eca477515f49efdcbe4dd3c56dc0893b77
Ondřej Zbitek <zbiteko23@spstrutnov.cz>
2024-01-18 10:14:10 +0100

    mam pozice ale ne čtverec

--- src/velký čverec žere malý.py
+++ src/velký čverec žere malý.py
@@ -3,51 +3,61 @@ import sys
 import pygame
 pygame.init()
 
-rozliseni_okna = (800, 600)
+import random
+
+#velikost okna
+rozliseni_okna = (800, 800)
 
 okno = pygame.display.set_mode(rozliseni_okna)
 
-pozice_micku_x = 300
-pozice_micku_y = 200
-velikost_micku = 50
-rychlost_micku_x = 0.5
-rychlost_micku_y = 0.5
+#pozice ctverce
+pozice_ctverce_x = 500
+pozice_ctverce_y = 100
 
-pozice_hrace_x = 200
-pozice_hrace_y = 100
-rychlost_hrace = 0.2
+#ryclost ctverce
+rychlost_ctverce = 0.2
 
+#nahodna pozice ctevercku
+maly_x = random.randint(20, 780)
+maly_y = random.randint(20, 780)
+
+
+
+#ukonceni hry
 while True:
     for udalost in pygame.event.get():
         if udalost.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    
+            
+#pohyb ctverce
     stisknute_klavesy = pygame.key.get_pressed()
     
     if stisknute_klavesy[pygame.K_RIGHT]:
-        pozice_hrace_x += rychlost_hrace
+        pozice_ctverce_x += rychlost_ctverce
+        
+    if stisknute_klavesy[pygame.K_LEFT]:
+        pozice_ctverce_x -= rychlost_ctverce
+        
+    if stisknute_klavesy[pygame.K_UP]:
+        pozice_ctverce_y -= rychlost_ctverce
+        
+    if stisknute_klavesy[pygame.K_DOWN]:
+        pozice_ctverce_y += rychlost_ctverce
+            
+#vytvoreni malych cteverku
+    pygame.draw.rect(okno, (255, 0, 0), (maly_x, maly_y, 15, 15))
+        
     
-    pozice_micku_x += rychlost_micku_x
-    pozice_micku_y += rychlost_micku_y
     
-    if pozice_micku_x < 0:
-        pozice_micku_x = 0
-        rychlost_micku_x *= -1
-    if pozice_micku_y < 0:
-        pozice_micku_y = 0
-        rychlost_micku_y *= -1
-    
-    if pozice_micku_x > rozliseni_okna[0] - velikost_micku:
-        pozice_micku_x = rozliseni_okna[0] - velikost_micku
-        rychlost_micku_x *= -1
-    if pozice_micku_y > rozliseni_okna[1] - velikost_micku:
-        pozice_micku_y = rozliseni_okna[1] - velikost_micku
-        rychlost_micku_y *= -1
-    
+#barva okna
     okno.fill((255, 255, 255))
     
-    pygame.draw.rect(okno, (0, 0, 0), (pozice_hrace_x, pozice_hrace_y, 50, 50))
-    pygame.draw.ellipse(okno, (0, 0, 255), (pozice_micku_x, pozice_micku_y, velikost_micku, velikost_micku))
+#vytvoreni ctverce
+    pygame.draw.rect(okno, (0, 0, 0), (pozice_ctverce_x, pozice_ctverce_y, 20, 20))
+    
+#vytvoreni malych ctvercu
+    
     
     pygame.display.update()
+
